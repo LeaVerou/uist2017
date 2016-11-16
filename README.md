009fe52872b9cf8e78094cacad4fe57a5b243701
Lea Verou <lea@verou.me>
2016-11-16 00:23:30 -0500

    Header, npm, gulp. Header image is currently a preview from Adobe Stock.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # uist2017
 Website for ACM UIST 2017
+
+## Editing
+
+Before making any CSS edits:
+
+- First, install Node and npm if you don't already have it
+- `cd` to the website folder and run `npm install`
+
+After that, run `gulp watch` before you make any edits, and it will automatically generate any files needed.
+
+For now, `gulp` is only used to preprocess CSS.
+Once the website has multiple pages, it will also be used for templating, so the process above will be needed for editing HTML too.
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+/*
+Build file to concat & minify files, compile SCSS and so on.
+*/
+// grab our gulp packages
+var gulp  = require("gulp");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+var fileinclude = require("gulp-file-include");
+var notify = require("gulp-notify");
+
+gulp.task("sass", function() {
+	return gulp.src(["**/*.scss", "!node_modules/**"])
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer({
+			browsers: ["last 2 versions"],
+			cascade: false
+		}))
+		.pipe(rename({ extname: ".css" }))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("."))
+		.pipe(notify("<%= file.relative %> done!"));
+});
+
+// gulp.task("update", function() {
+// 	gulp.src(["../mavo/dist/**/*"]).pipe(gulp.dest("mavo"));
+// });
+
+// gulp.task("html", function() {
+// 	gulp.src(["**/*.tpl.html"])
+// 		.pipe(fileinclude({
+// 			basepath: "templates/"
+// 		}).on("error", function(error) {
+// 			console.error(error);
+// 		}))
+// 		.pipe(rename({ extname: "" }))
+// 		.pipe(rename({ extname: ".html" }))
+// 		.pipe(gulp.dest("."))
+// 		.pipe(notify("<%= file.relative %> done!"));
+// });
+
+gulp.task("watch", function() {
+	//gulp.watch(["../mavo/dist/*"], ["update"]);
+	gulp.watch(["**/*.scss"], ["sass"]);
+	//gulp.watch(["**/*.tpl.html", "./templates/*.html"], ["html"]);
+});
+
+gulp.task("default", ["sass"]);
